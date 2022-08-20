1a4b83c9b412394ab344f0504cff7c476cc1ec54
Melodi <62381002+Melodi17@users.noreply.github.com>
2022-08-20 13:29:17 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Net;
+using CommandLine;
+
+namespace dexec
+{
+    public class Options
+    {
+        [Value(0, MetaName = "file", HelpText = "File/url to download", Required = true)]
+        public string File { get; set; }
+
+        //[Option('p', "persist", HelpText = "Whether the file should persist after execution", Default = false)]
+        //public bool Persist { get; set; }
+
+        //[Option('e', "elevated", HelpText = "Whether the file should be elevated during execution", Default = false)]
+        //public bool Elevated { get; set; }
+    }
+
+    internal class Program
+    {
+        static Options opt = null;
+        static string localFileName;
+        static void Main(string[] args)
+        {
+            string[] safeArgs = Box.IOUtils.ConnectStrings(string.Join(' ', args), false);
+            Parser.Default.ParseArguments<Options>(safeArgs).WithParsed(o => opt = o);
+            
+            if (opt == null)
+                return;
+            
+            WebClient client = new();
+            localFileName = Path.GetFileName(opt.File);
+            client.DownloadFile(opt.File, localFileName);
+            Process p = new()
+            {
+                StartInfo = new()
+                {
+                    WorkingDirectory = Environment.CurrentDirectory,
+                    FileName = "explorer",
+                    Arguments = $" \"{localFileName}\"",
+                    UseShellExecute = true,
+                }
+            };
+            p.Start();
+        }
+    }
+}
